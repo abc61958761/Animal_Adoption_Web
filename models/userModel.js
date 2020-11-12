859234e67c2b51ace45f200179e69ba97b228f00
Liv Huang <livhuang95@gmail.com>
2020-11-12 20:59:37 +0800

    feat: add user model

---
+++ models/userModel.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 20,
+    minlength: 2
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, '格式錯誤']
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    required: true,
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: [12, '請輸入8-12字元'],
+    minlength: [8, '請輸入8-12字元'],
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: '密碼不相符'
+    }
+  },
+  passwordChangedAt: {
+    type: Date
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  this.confirmPassword = undefined;
+});
+
+userSchema.methods.comparePassword = async function(
+  inputPassword,
+  userPassword
+) {
+  return await bcrypt.compare(inputPassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
